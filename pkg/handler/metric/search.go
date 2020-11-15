75c726884f371efea02dc1e56b4742346df89cf1
Tim Schindler <xh3b4sd@gmail.com>
2020-11-15 17:46:57 +0100

    Revert "debug"
    This reverts commit aaa4c24fa089cc4afe3aceaeaa6ee2739604d16e.

--- pkg/handler/metric/search.go
+++ pkg/handler/metric/search.go
@@ -2,71 +2,12 @@ package metric
 
 import (
 	"context"
-	"net"
 
 	"github.com/venturemark/apigengo/pkg/pbf/metric"
 	"github.com/xh3b4sd/tracer"
 )
 
 func (h *Handler) Search(ctx context.Context, obj *metric.SearchI) (*metric.SearchO, error) {
-	{
-		var err error
-		var res []string
-
-		{
-			host := "google.com"
-
-			h.logger.Log(ctx, "level", "info", "message", "resolving host", "host", host)
-			res, err = net.LookupHost(host)
-			if err != nil {
-				h.logger.Log(ctx, "level", "error", "message", "resolving failed", "stack", tracer.JSON(err), "host", host)
-			}
-
-			for _, a := range res {
-				h.logger.Log(ctx, "level", "info", "message", "resolved host", "host", host, "address", a)
-			}
-		}
-		{
-			host := "www.google.com"
-
-			h.logger.Log(ctx, "level", "info", "message", "resolving host", "host", host)
-			res, err = net.LookupHost(host)
-			if err != nil {
-				h.logger.Log(ctx, "level", "error", "message", "resolving failed", "stack", tracer.JSON(err), "host", host)
-			}
-
-			for _, a := range res {
-				h.logger.Log(ctx, "level", "info", "message", "resolved host", "host", host, "address", a)
-			}
-		}
-		{
-			host := "redis-master.infra.svc.cluster.local"
-
-			h.logger.Log(ctx, "level", "info", "message", "resolving host", "host", host)
-			res, err = net.LookupHost(host)
-			if err != nil {
-				h.logger.Log(ctx, "level", "error", "message", "resolving failed", "stack", tracer.JSON(err), "host", host)
-			}
-
-			for _, a := range res {
-				h.logger.Log(ctx, "level", "info", "message", "resolved host", "host", host, "address", a)
-			}
-		}
-		{
-			host := "cluster.local"
-
-			h.logger.Log(ctx, "level", "info", "message", "resolving host", "host", host)
-			res, err = net.LookupHost(host)
-			if err != nil {
-				h.logger.Log(ctx, "level", "error", "message", "resolving failed", "stack", tracer.JSON(err), "host", host)
-			}
-
-			for _, a := range res {
-				h.logger.Log(ctx, "level", "info", "message", "resolved host", "host", host, "address", a)
-			}
-		}
-	}
-
 	// Search for any metric associated with the given updates. One or many
 	// update IDs may be provided.
 	{
