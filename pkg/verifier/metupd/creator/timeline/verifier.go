0151afa21101689dacd3165412af9960f6a6478c
Tim Schindler <xh3b4sd@gmail.com>
2020-11-27 15:55:26 +0100

    prevent metric updates to be created in timelines that do not exist

---
+++ pkg/verifier/metupd/creator/timeline/verifier.go
@@ -0,0 +1,81 @@
+package timeline
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/venturemark/apigengo/pkg/pbf/metupd"
+	"github.com/xh3b4sd/redigo"
+	"github.com/xh3b4sd/tracer"
+
+	"github.com/venturemark/apiserver/pkg/key"
+	"github.com/venturemark/apiserver/pkg/metadata"
+)
+
+type VerifierConfig struct {
+	Redigo redigo.Interface
+}
+
+type Verifier struct {
+	redigo redigo.Interface
+}
+
+func NewVerifier(config VerifierConfig) (*Verifier, error) {
+	if config.Redigo == nil {
+		return nil, tracer.Maskf(invalidConfigError, "%T.Redigo must not be empty", config)
+	}
+
+	v := &Verifier{
+		redigo: config.Redigo,
+	}
+
+	return v, nil
+}
+
+// Verify checks if the timeline which the metric update should be created for
+// does even exist. Creating metric updates requires a timeline to exist.
+func (v *Verifier) Verify(req *metupd.CreateI) (bool, error) {
+	{
+		if req.Obj == nil {
+			return false, nil
+		}
+		if req.Obj.Metadata == nil {
+			return false, nil
+		}
+	}
+
+	var tml string
+	var usr string
+	{
+		tml = req.Obj.Metadata[metadata.TimelineID]
+		usr = req.Obj.Metadata[metadata.UserID]
+
+		if tml == "" {
+			return false, nil
+		}
+		if usr == "" {
+			return false, nil
+		}
+	}
+
+	{
+		i, err := strconv.ParseFloat(tml, 64)
+		if err != nil {
+			return false, tracer.Mask(err)
+		}
+
+		k := fmt.Sprintf(key.Timeline, usr)
+		s := i
+
+		exi, err := v.redigo.Scored().Exists(k, s)
+		if err != nil {
+			return false, tracer.Mask(err)
+		}
+
+		if !exi {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
