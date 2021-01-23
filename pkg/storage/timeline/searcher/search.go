2a3b84347786a04bcf822b6ea412bb16410a3d18
Tim Schindler <xh3b4sd@gmail.com>
2021-01-23 18:08:33 +0100

    add permission model

--- pkg/storage/timeline/searcher/search.go
+++ pkg/storage/timeline/searcher/search.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/venturemark/apigengo/pkg/pbf/audience"
 	"github.com/venturemark/apigengo/pkg/pbf/timeline"
 	"github.com/xh3b4sd/tracer"
 
@@ -22,14 +23,25 @@ func (s *Searcher) Search(req *timeline.SearchI) (*timeline.SearchO, error) {
 		oid = req.Obj[0].Metadata[metadata.OrganizationID]
 	}
 
-	// With redis we use ZREVRANGE which allows us to search for objects while
-	// having support for chunking.
 	var str []string
 	{
-		k := fmt.Sprintf(key.Timeline, oid)
-		str, err = s.redigo.Sorted().Search().Index(k, 0, -1)
-		if err != nil {
-			return nil, tracer.Mask(err)
+		met := map[string]string{
+			metadata.PermissionID:     "audience",
+			metadata.PermissionStatus: "enabled",
+		}
+
+		con := metadata.Contains(req.Obj[0].Metadata, met)
+
+		if con {
+			str, err = s.searchUsr(req)
+			if err != nil {
+				return nil, tracer.Mask(err)
+			}
+		} else {
+			str, err = s.searchAll(req)
+			if err != nil {
+				return nil, tracer.Mask(err)
+			}
 		}
 	}
 
@@ -63,4 +75,92 @@ func (s *Searcher) Search(req *timeline.SearchI) (*timeline.SearchO, error) {
 	}
 
 	return res, nil
+}
+
+func (s *Searcher) searchAll(req *timeline.SearchI) ([]string, error) {
+	var err error
+
+	var oid string
+	{
+		oid = req.Obj[0].Metadata[metadata.OrganizationID]
+	}
+
+	// With redis we use ZREVRANGE which allows us to search for objects while
+	// having support for chunking.
+	var str []string
+	{
+		k := fmt.Sprintf(key.Timeline, oid)
+		str, err = s.redigo.Sorted().Search().Index(k, 0, -1)
+		if err != nil {
+			return nil, tracer.Mask(err)
+		}
+	}
+
+	return str, nil
+}
+
+func (s *Searcher) searchUsr(req *timeline.SearchI) ([]string, error) {
+	var err error
+
+	var oid string
+	{
+		oid = req.Obj[0].Metadata[metadata.OrganizationID]
+	}
+
+	var uid string
+	{
+		uid = req.Obj[0].Metadata[metadata.UserID]
+	}
+
+	var aud *audience.SearchO
+	{
+		aud, err = s.searchAud(req)
+		if err != nil {
+			return nil, tracer.Mask(err)
+		}
+	}
+
+	var tim []string
+	{
+		for _, o := range aud.Obj {
+			if !contains(o.Property.User, uid) {
+				continue
+			}
+
+			tim = append(tim, o.Property.Tmln...)
+		}
+	}
+
+	var res []string
+	{
+		for _, t := range tim {
+			var tid float64
+			{
+				tid, err = strconv.ParseFloat(t, 64)
+				if err != nil {
+					return nil, tracer.Mask(err)
+				}
+			}
+
+			k := fmt.Sprintf(key.Timeline, oid)
+			str, err := s.redigo.Sorted().Search().Score(k, tid, tid)
+			if err != nil {
+				return nil, tracer.Mask(err)
+			}
+
+			res = append(res, str...)
+		}
+	}
+
+	return res, nil
+}
+
+func contains(all []string, sub string) bool {
+	for _, a := range all {
+		if a == sub {
+			return true
+		}
+	}
+
+	return false
 }
