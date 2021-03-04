ec1377c5418f2122ae5769b454358b8ace7242d9
Tim Schindler <xh3b4sd@gmail.com>
2021-03-04 21:59:52 +0100

    update gomod files

--- pkg/storage/update/searcher/search.go
+++ pkg/storage/update/searcher/search.go
@@ -31,7 +31,7 @@ func (s *Searcher) Search(req *update.SearchI) (*update.SearchO, error) {
 	var str []string
 	{
 		k := fmt.Sprintf(key.Update, oid, tid)
-		str, err = s.redigo.Sorted().Search().Index(k, 0, -1)
+		str, err = s.redigo.Sorted().Search().Order(k, 0, -1)
 		if err != nil {
 			return nil, tracer.Mask(err)
 		}
