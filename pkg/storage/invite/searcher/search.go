33495bc131b6f377e2a4363fa2159bc7a8c3146e
Tim Schindler <xh3b4sd@gmail.com>
2021-03-21 22:06:40 +0100

    search invites by email (#166)

--- pkg/storage/invite/searcher/search.go
+++ pkg/storage/invite/searcher/search.go
@@ -3,7 +3,9 @@ package searcher
 import (
 	"encoding/json"
 
+	"github.com/venturemark/apicommon/pkg/index"
 	"github.com/venturemark/apicommon/pkg/key"
+	"github.com/venturemark/apicommon/pkg/metadata"
 	"github.com/venturemark/apicommon/pkg/schema"
 	"github.com/venturemark/apigengo/pkg/pbf/invite"
 	"github.com/xh3b4sd/tracer"
@@ -12,18 +14,22 @@ import (
 func (s *Searcher) Search(req *invite.SearchI) (*invite.SearchO, error) {
 	var err error
 
-	var ink *key.Key
-	{
-		ink = key.Invite(req.Obj[0].Metadata)
-	}
-
 	var str []string
 	{
-		k := ink.List()
+		sub := req.Obj[0].Metadata[metadata.SubjectEmail] != ""
 
-		str, err = s.redigo.Sorted().Search().Order(k, 0, -1)
-		if err != nil {
-			return nil, tracer.Mask(err)
+		if sub {
+			str, err = s.searchSub(req)
+			if err != nil {
+				return nil, tracer.Mask(err)
+			}
+		}
+
+		if !sub {
+			str, err = s.searchVen(req)
+			if err != nil {
+				return nil, tracer.Mask(err)
+			}
 		}
 	}
 
@@ -51,4 +57,49 @@ func (s *Searcher) Search(req *invite.SearchI) (*invite.SearchO, error) {
 	}
 
 	return res, nil
+}
+
+func (s *Searcher) searchSub(req *invite.SearchI) ([]string, error) {
+	var ink *key.Key
+	{
+		ink = key.Invite(req.Obj[0].Metadata)
+	}
+
+	var str []string
+	{
+		k := ink.List()
+		i := index.New(index.Mail, req.Obj[0].Metadata[metadata.SubjectEmail])
+
+		s, err := s.redigo.Sorted().Search().Index(k, i)
+		if err != nil {
+			return nil, tracer.Mask(err)
+		}
+
+		if s != "" {
+			str = append(str, s)
+		}
+	}
+
+	return str, nil
+}
+
+func (s *Searcher) searchVen(req *invite.SearchI) ([]string, error) {
+	var err error
+
+	var ink *key.Key
+	{
+		ink = key.Invite(req.Obj[0].Metadata)
+	}
+
+	var str []string
+	{
+		k := ink.List()
+
+		str, err = s.redigo.Sorted().Search().Order(k, 0, -1)
+		if err != nil {
+			return nil, tracer.Mask(err)
+		}
+	}
+
+	return str, nil
 }
