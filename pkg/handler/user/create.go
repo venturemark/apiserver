f9bc76b7bb7bd7d66dfa30b992ebc9209463a7e8
Thomas Fussell <thomas@fussell.io>
2022-01-22 12:37:25 -0600

    implement new user onboarding

--- pkg/handler/user/create.go
+++ pkg/handler/user/create.go
@@ -2,12 +2,14 @@ package user
 
 import (
 	"context"
+	"github.com/venturemark/apigengo/pkg/pbf/timeline"
 	"strconv"
 	"time"
 
 	"github.com/venturemark/apicommon/pkg/metadata"
 	"github.com/venturemark/apigengo/pkg/pbf/role"
 	"github.com/venturemark/apigengo/pkg/pbf/user"
+	"github.com/venturemark/apigengo/pkg/pbf/venture"
 	"github.com/xh3b4sd/tracer"
 
 	"github.com/venturemark/apiserver/pkg/context/claimid"
@@ -70,6 +72,7 @@ func (h *Handler) Create(ctx context.Context, req *user.CreateI) (*user.CreateO,
 		}
 	}
 
+	var res *user.CreateO
 	{
 		ok, err := h.storage.User.Creator.Verify(ctx, req)
 		if err != nil {
@@ -80,11 +83,182 @@ func (h *Handler) Create(ctx context.Context, req *user.CreateI) (*user.CreateO,
 			return nil, tracer.Mask(invalidInputError)
 		}
 
-		res, err := h.storage.User.Creator.Create(req)
+		res, err = h.storage.User.Creator.Create(req)
 		if err != nil {
 			return nil, tracer.Mask(err)
 		}
-
-		return res, nil
 	}
+
+	for i := range res.Obj {
+		createdUser := res.Obj[i]
+		usi := createdUser.Metadata[metadata.UserID]
+		userRequest := req.Obj[i]
+		err := h.createDefaultTimelines(ctx, userRequest, usi)
+		if err != nil {
+			return nil, tracer.Mask(err)
+		}
+	}
+
+	return res, nil
+}
+
+var defaultVenture = venture.CreateI_Obj_Property{
+	Desc: "x",
+	Name: "x",
+}
+
+var defaultTimelinesMap = map[string][]timeline.CreateI_Obj_Property{
+	"a": {
+		{
+			Desc: "a",
+			Name: "a",
+		},
+	},
+	"b": {
+		{
+			Desc: "b",
+			Name: "b",
+		},
+	},
+}
+
+func (h *Handler) createDefaultTimelines(ctx context.Context, userRequest *user.CreateI_Obj, usi string) error {
+	defaultTimelines := defaultTimelinesMap[userRequest.Metadata["user.venturemark.co/prepopulate"]]
+
+	if len(defaultTimelines) == 0 {
+		return nil
+	}
+
+	ventureRoleID := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+	ventureID := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+
+	ventureMetadata := map[string]string{
+		metadata.SubjectID:    usi,
+		metadata.UserID:       usi,
+		metadata.ResourceKind: "venture",
+		metadata.RoleKind:     "owner",
+		metadata.RoleID:       ventureRoleID,
+		metadata.VentureID:    ventureID,
+	}
+
+	{
+		rolReq := role.CreateI{
+			Obj: []*role.CreateI_Obj{
+				{
+					Metadata: ventureMetadata,
+				},
+			},
+		}
+
+		ok, err := h.storage.Role.Creator.Verify(ctx, &rolReq)
+		if err != nil {
+			return tracer.Mask(err)
+		}
+
+		if !ok {
+			return tracer.Mask(invalidInputError)
+		}
+
+		_, err = h.storage.Role.Creator.Create(&rolReq)
+		if err != nil {
+			return tracer.Mask(err)
+		}
+	}
+
+	{
+		ventureReq := venture.CreateI{
+			Obj: []*venture.CreateI_Obj{
+				{
+					Metadata: ventureMetadata,
+					Property: &venture.CreateI_Obj_Property{
+						Desc: defaultVenture.Desc,
+						Name: defaultVenture.Name,
+					},
+				},
+			},
+		}
+
+		ok, err := h.storage.Venture.Creator.Verify(ctx, &ventureReq)
+		if err != nil {
+			return tracer.Mask(err)
+		}
+
+		if !ok {
+			return tracer.Mask(invalidInputError)
+		}
+
+		_, err = h.storage.Venture.Creator.Create(&ventureReq)
+		if err != nil {
+			return tracer.Mask(err)
+		}
+	}
+
+	for _, defaultTimeline := range defaultTimelines {
+		timelineRoleID := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+		timelineID := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+
+		timelineMetadata := map[string]string{
+			metadata.SubjectID:    usi,
+			metadata.UserID:       usi,
+			metadata.ResourceKind: "timeline",
+			metadata.RoleKind:     "owner",
+			metadata.RoleID:       timelineRoleID,
+			metadata.TimelineID:   timelineID,
+			metadata.VentureID:    ventureID,
+		}
+
+		{
+			rolReq := role.CreateI{
+				Obj: []*role.CreateI_Obj{
+					{
+						Metadata: timelineMetadata,
+					},
+				},
+			}
+
+			ok, err := h.storage.Role.Creator.Verify(ctx, &rolReq)
+			if err != nil {
+				return tracer.Mask(err)
+			}
+
+			if !ok {
+				return tracer.Mask(invalidInputError)
+			}
+
+			_, err = h.storage.Role.Creator.Create(&rolReq)
+			if err != nil {
+				return tracer.Mask(err)
+			}
+		}
+
+		{
+			timelineReq := timeline.CreateI{
+				Obj: []*timeline.CreateI_Obj{
+					{
+						Metadata: timelineMetadata,
+						Property: &timeline.CreateI_Obj_Property{
+							Desc: defaultTimeline.Desc,
+							Name: defaultTimeline.Name,
+						},
+					},
+				},
+			}
+
+			ok, err := h.storage.Timeline.Creator.Verify(ctx, &timelineReq)
+			if err != nil {
+				return tracer.Mask(err)
+			}
+
+			if !ok {
+				return tracer.Mask(invalidInputError)
+			}
+
+			_, err = h.storage.Timeline.Creator.Create(&timelineReq)
+			if err != nil {
+				return tracer.Mask(err)
+			}
+		}
+	}
+
+	return nil
 }
--- pkg/verifier/user/creator/format/verifier.go
+++ pkg/verifier/user/creator/format/verifier.go
@@ -19,7 +19,27 @@ func NewVerifier(config VerifierConfig) (*Verifier, error) {
 	return v, nil
 }
 
+func contains(s string, a []string) bool {
+	for _, v := range a {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+var allowedPrepopulate = []string{"a", "b"}
+var allowedSurvey = []string{"x", "y"}
+
 func (v *Verifier) Verify(ctx context.Context, req *user.CreateI) (bool, error) {
+	if prepopulate := req.Obj[0].Metadata["user.venturemark.co/prepopulate"]; !contains(prepopulate, allowedPrepopulate) {
+		return false, nil
+	}
+
+	if survey := req.Obj[0].Metadata["user.venturemark.co/surveyResponse"]; !contains(survey, allowedSurvey) {
+		return false, nil
+	}
+
 	{
 		err := checkmail.ValidateFormat(req.Obj[0].Property.Mail)
 		if err != nil {
